0ab0bd42292534b227ade30e74d85e6504209471
Srikanth Palagani <india2sree@gmail.com>
2020-02-03 15:01:55 +0530

    update react router dom and material ui

--- usermanagmentsystem/src/App.js
+++ usermanagmentsystem/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+import login from './pages/login';
+import home from './pages/home';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="container">
+        <Switch>
+          <Route exact path="/" component={home}/>
+          <Route exact path="/login" component={login}/>
+          <Route exact path="/signup" component={signup}/>
+        </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
